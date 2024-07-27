ce2b57c878858c3bc8d5febe653a5096900ae4c4
TWSho <twsho1996@gmail.com>
2024-07-27 18:45:59 +0900

    Initial commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,14 @@
+// src/components/MainContent.js
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main>
+      <h3>友達の性格タイプを簡単に記録できる</h3>
+      <h3>シンプルなアプリ</h3>
+      
+    </main>
+  );
+};
+
+export default MainContent;
