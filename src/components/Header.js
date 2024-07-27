2418da260956abb0dc95b33bbdb89fc2bb951414
TWSho <twsho1996@gmail.com>
2024-07-27 23:30:51 +0900

    feat: アイコン追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 // src/components/Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import icon from '../assets/icon_t.png';
+
 
 const Header = () => {
   return (
     <header>
-      <nav>
-        <Link to="/type-memo-page" className="home-link">Type Memo</Link>
+      <nav className="nav-container">
+        <img src={icon} alt="Type Memo Icon" className="header-icon" />
+        <Link to="/" className="home-link">Type Memo</Link>
       </nav>
     </header>
   );
